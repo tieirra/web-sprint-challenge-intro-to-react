76ed39c94d0ee9b846ca55dc15bd15e58714f1c2
user <user@users-MacBook-Air.local>
2020-07-17 11:07:40 -0500

    adding styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
+import People  from "./components/Character"; 
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +12,27 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people')
+      .then((resp) => {
+        console.log(resp);
+        setCharacters(resp.data.results);
+      })
+
+      .catch((error) => {
+        console.log(error);
+
+      })
+
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="cardHolder">
+        <People data={characters} />
+      </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,64 @@
 // Write your Character component here
+
+import React, { useState } from "react";
+import styled from 'styled-components';
+import App from "../App"
+
+function People(props) {
+
+
+  return (
+
+    <div> {props.data.map((data, i) => {
+      return (
+
+        <Card key={i}>
+          <h1>Name: {data.name}</h1>
+          <h3>Eye color: {data.eye_color}</h3>
+
+          <img src={data.image} />
+        </Card>
+
+      );
+
+
+    })}
+
+    </div>
+  );
+
+
+};
+
+
+
+
+const div = styled.div`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  display: flex;
+  align-items: center;
+`;
+
+
+const Card = styled.div`
+  background-color: #fff;
+  border: solid 2px hotpink;
+  border-radius: 2%;
+  width: 55%;
+  margin-left: 15%;
+  padding: 2%;
+  box-shadow: 5px 5px 20px pink;
+  color: #555;
+`;
+
+
+const img = styled.img`
+  border: gray solid 3px;
+  border-radius: 5%;
+  max-width: 100%;
+`;
+
+
+export default People;
